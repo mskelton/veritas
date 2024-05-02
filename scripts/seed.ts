030cbb8d76424ec3f17cb279f2599288493f0af9
Mark Skelton <mdskelton99@gmail.com>
2024-05-02 07:39:53 -0500

    Seed

---
+++ scripts/seed.ts
@@ -0,0 +1,40 @@
+import { db, schema } from "../src/app/lib/db"
+import { encrypt } from "../src/app/lib/encryption"
+
+await db.insert(schema.dataSources).values({
+  database: "test-postgres-a",
+  description: "Test Postgres A",
+  host: "localhost",
+  id: crypto.randomUUID(),
+  name: "Test A",
+  password: encrypt("password-a"),
+  port: 5433,
+  type: "postgres",
+  username: "test-a",
+})
+
+await db.insert(schema.dataSources).values({
+  database: "test-postgres-b",
+  description: "Test Postgres B",
+  host: "localhost",
+  id: crypto.randomUUID(),
+  name: "Test B",
+  password: encrypt("password-b"),
+  port: 5434,
+  type: "postgres",
+  username: "test-b",
+})
+
+await db.insert(schema.facts).values({
+  id: crypto.randomUUID(),
+  name: "Has Authors",
+  query: "select * from authors",
+  type: "boolean",
+})
+
+await db.insert(schema.facts).values({
+  id: crypto.randomUUID(),
+  name: "Authors starting with 'M'",
+  query: "select count(*) from authors\nwhere name like 'M%'",
+  type: "count",
+})
