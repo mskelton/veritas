b8994f8aa758fc83b01660bc9a27ca288f8200d6
Mark Skelton <mdskelton99@gmail.com>
2024-05-27 16:54:22 -0500

    Fix clients

--- src/app/lib/db/index.ts
+++ src/app/lib/db/index.ts
@@ -2,7 +2,22 @@ import "server-only"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
-export const client = postgres(process.env.DATABASE_URL!)
+declare let globalThis: {
+  client?: ReturnType<typeof postgres>
+}
+
+export let client: ReturnType<typeof postgres>
+
+if (process.env.NODE_ENV !== "production") {
+  if (!globalThis.client) {
+    globalThis.client = postgres(process.env.DATABASE_URL!)
+  }
+
+  client = globalThis.client
+} else {
+  client = postgres(process.env.DATABASE_URL!)
+}
+
 export const db = drizzle(client)
 
 export * as schema from "./schema"
